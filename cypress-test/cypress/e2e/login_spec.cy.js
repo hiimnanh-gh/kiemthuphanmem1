fd67fd9ed7b483d740ffe29d27e95317f5bbd01a
Đào Nguyên Anh <daonguyenanhprivate@gmail.com>
2026-01-19 00:33:14 +0700

    test: implement login scenarios and fix password selector error closes #5

--- cypress-test/cypress/e2e/login_spec.cy.js
+++ cypress-test/cypress/e2e/login_spec.cy.js
@@ -1,19 +1,29 @@
 describe("Login Test", () => {
+  // beforeEach chạy trước mỗi bài test để đảm bảo môi trường sạch
+  beforeEach(() => {
+    // Xóa sạch session và cookies để tránh bị redirect nhầm trang
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    // Luôn truy cập trang chủ trước khi làm bất cứ việc gì
+    cy.visit("https://www.saucedemo.com");
+  });
+
   // Kịch bản 1: Đăng nhập thành công
   it("Should login successfully with valid credentials", () => {
-    cy.visit("https://www.saucedemo.com");
     cy.get("#user-name").type("standard_user");
     cy.get("#password").type("secret_sauce");
     cy.get("#login-button").click();
+    // Xác minh đã chuyển đến trang kho hàng
     cy.url().should("include", "/inventory.html");
   });
 
   // Kịch bản 2: Đăng nhập thất bại
   it("Should show error message with invalid credentials", () => {
-    cy.visit("https://www.saucedemo.com");
     cy.get("#user-name").type("invalid_user");
     cy.get("#password").type("wrong_password");
     cy.get("#login-button").click();
+
+    // Kiểm tra container thông báo lỗi có chứa nội dung đúng yêu cầu
     cy.get(".error-message-container").should(
       "contain",
       "Username and password do not match",
