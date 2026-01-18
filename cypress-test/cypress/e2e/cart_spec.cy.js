b86ab8ce0e216e2e0e8a2ca9efa9e6756e40595f
Đào Nguyên Anh <daonguyenanhprivate@gmail.com>
2026-01-19 00:33:35 +0700

    test: implement add/remove cart and checkout process scenarios closes #6

--- cypress-test/cypress/e2e/cart_spec.cy.js
+++ cypress-test/cypress/e2e/cart_spec.cy.js
@@ -6,38 +6,30 @@ describe("Cart and Checkout Test", () => {
     cy.get("#login-button").click();
   });
 
-  // Kịch bản 3: Thêm sản phẩm
   it("Should add a product to the cart", () => {
     cy.get(".inventory_item").first().find(".btn_inventory").click();
-    cy.get(".shopping_cart_badge").should("have.text", "1");
+    cy.get(".shopping_cart_badge").should("have.text", "1"); // Kịch bản 3
   });
 
-  // Kịch bản 4: Sắp xếp giá
   it("Should sort products by price low to high", () => {
     cy.get(".product_sort_container").select("lohi");
-    cy.get(".inventory_item_price").first().should("have.text", "$7.99");
+    cy.get(".inventory_item_price").first().should("have.text", "$7.99"); // Kịch bản 4
   });
 
-  // Yêu cầu thêm 1: Xóa sản phẩm
   it("Should remove a product from the cart", () => {
-    cy.get(".inventory_item").first().find(".btn_inventory").click(); // Thêm vào trước
+    cy.get(".inventory_item").first().find(".btn_inventory").click(); // Thêm vào
     cy.get(".btn_inventory").contains("Remove").click(); // Nhấn nút Remove
-    cy.get(".shopping_cart_badge").should("not.exist"); // Badge biến mất
+    cy.get(".shopping_cart_badge").should("not.exist"); // Xác minh giỏ hàng trống
   });
 
-  // Yêu cầu thêm 2: Quy trình thanh toán (Checkout)
-  it("Should complete the checkout flow", () => {
+  it("Should complete the checkout process", () => {
     cy.get(".inventory_item").first().find(".btn_inventory").click();
     cy.get(".shopping_cart_link").click();
     cy.get("#checkout").click();
-
-    // Điền thông tin khách hàng
-    cy.get("#first-name").type("John");
+    cy.get("#first-name").type("John"); // Điền thông tin
     cy.get("#last-name").type("Doe");
     cy.get("#postal-code").type("12345");
     cy.get("#continue").click();
-
-    // Xác minh đến trang xác nhận cuối cùng
-    cy.url().should("include", "/checkout-step-two.html");
+    cy.url().should("include", "/checkout-step-two.html"); // Xác minh thanh toán
   });
 });
